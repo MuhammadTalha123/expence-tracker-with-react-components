a2e68f24d327c35ee2f4cf869bf452a7fd1c7563
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-03 00:36:48 +0500

    change in transaction list

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -4,8 +4,10 @@ export const TransactionList = () => {
     return (
         <div>
             <h3>History</h3>
-            <ul className='minus'>
+            <ul className='list'>
+                <li className='minus'>
                 Cash <span>-$400</span><button className='delete-btn'>x</button>
+                </li>
             </ul>
         </div>
     )
